26c61daff9b9383da5b53955c38c309d3c2655a1
alanmathew333 <mathewalan84@gmail.com>
2018-07-17 17:01:49 +0530

    automated file creation

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ const exec = require('child_process').exec // to execute commands
 let listfunction = () => {
 
   // questions
-  console.log('\n---------------{ nem }---------------\n')
+  console.log('\n-{ N E M }- Node-Express-MongoDb -')
+  console.log('\nPlease answer the following :-\n')
   inquirer.prompt([{
     name: 'name',
     type: 'input',
-    message: 'Tell me the name of your project :'
+    message: 'What is the name of your project? :'
   }, {
     name: 'description',
     type: 'input',
@@ -23,7 +24,7 @@ let listfunction = () => {
   }, {
     name: 'author',
     type: 'input',
-    message: 'So who is the author? :'
+    message: 'Name the author :'
   }, {
     name: 'version',
     type: 'input',
@@ -31,11 +32,11 @@ let listfunction = () => {
   }, {
     name: 'license',
     type: 'input',
-    message: 'Can you please specify the license :'
+    message: 'Kindly specify the license (MIT) :'
   }, {
     name: 'dependencies',
     type: 'checkbox',
-    message: 'Which all dependencies do you need : ',
+    message: 'Which all dependencies do you need :-',
     choices: [{
       name: 'express',
       checked: true
@@ -58,14 +59,14 @@ let listfunction = () => {
   }, {
     name: 'devdependencies',
     type: 'checkbox',
-    message: 'Specify the dev dependencies',
+    message: 'Specify the dev dependencies :-',
     choices: [{
       name: 'nodemon'
     }]
   }, {
     name: 'confirmation',
     type: 'confirm',
-    message: 'Do you wish to continue'
+    message: 'Do you wish to continue :'
   }]).then((answers) => {
 
 
@@ -75,7 +76,8 @@ let listfunction = () => {
     })
 
     // Adding dependencies
-    console.log('\n---------------{ nem }---------------\n')
+    console.log('\nEnter your password to install dependencies :-\n')
+
     for (var i = 0; answers.dependencies[i] + '' != 'undefined'; i++) {
 
       const cmd = 'sudo npm install --save ' + answers.dependencies[i]
@@ -86,6 +88,9 @@ let listfunction = () => {
       }
       exec(cmd, errfun)
     }
+
+
+    // creating all directories
     const cmd = 'mkdir routes models controller config utils services'
     let errfun = (error, stdout, stderr) => {
       if (error) console.log('exec error: ' + error)
@@ -95,27 +100,74 @@ let listfunction = () => {
     exec(cmd, errfun)
 
     // creating server.js
-    fs.writeFile('server.js', 'const express = require(\'express\');\nconst bodyParser = require(\'body-parser\');\nconst cors = require(\'cors\');\nconst morgan = require(\'morgan\');\nconst mongoose = require(\'mongoose\');\nconst db = require(\'./config/db.js\');\n\nconst port = process.env.PORT || 8083;\n\nconst app = express();\n\napp.use(bodyParser.json());\napp.use(morgan(\'dev\'));\napp.use(cors());\n\nmongoose.connect(db.url);\n\nrequire(\'./routes/index.js\')(app);\n\napp.listen(port);\nconsole.log(`Server Running on Port: ${port}`);', function (err) {
+    fs.writeFile('server.js', 'const express = require(\'express\');\nconst bodyParser = require(\'body-parser\');\nconst cors = require(\'cors\');\nconst morgan = require(\'morgan\');\nconst mongoose = require(\'mongoose\');\nconst db = require(\'./config/db.js\');\n\nconst port = process.env.PORT || 8083;\n\nconst app = express();\n\napp.use(bodyParser.json());\napp.use(morgan(\'dev\'));\napp.use(cors());\n\nmongoose.connect(db.url);\n\n\napp.listen(port, () => {\n  console.log(`Server is up in port : ` + port);\n})', function (err) {
       if (err) throw err;
     })
 
-    /*
+
     // creating api.js in routes directory
-    const cmd = 'cd routes'
-    let errfun = (error, stdout, stderr) => {
+
+    const cm = 'cd routes'
+    let errorfun = (error, stdout, stderr) => {
       if (error) console.log('exec error: ' + error)
       if (stdout) console.log(stdout)
+      //if (stderr) console.log('shell error: ' + stderr)
     }
-    exec(cmd, errfun)
+    exec(cm, errorfun)
+
+    fs.writeFile('./routes/api.js', 'const express = require(\'express\')\nconst Person = require(\'../models/person\')\n\nvar app = express()\n\napp.get(\'/person\', (req, res, next) => {\n  res.send({name: \'GET\'})\n})\n\napp.post(\'/person\', (req, res,next) => {\n  res.send({name: \'POST\'})\n})\n\napp.put(\'/person/:id\', (req, res, next) => {\n  res.send({name: \'PUT\'})\n})\n\napp.delete(\'/person/:id\', (req, res, next) => {\n  res.send({name: \'DELETE\'})\n})\n\nmodule.exports = router', (err) => {
+      if (err) {
+        throw err
+      }
+    })
 
     // creating schema in models directory
-    const cmd = 'cd ..'
-    let errfun = (error, stdout, stderr) => {
+    const cmm = 'cd ..'
+    let errrfunction = (error, stdout, stderr) => {
       if (error) console.log('exec error: ' + error)
       if (stdout) console.log(stdout)
+      //if (stderr) console.log('shell error: ' + stderr)
     }
-    exec(cmd, errfun)
-    */
+    exec(cmm, errrfunction)
+
+    const cmdd = 'cd models'
+    let errorfunction = (error, stdout, stderr) => {
+      if (error) console.log('exec error: ' + error)
+      if (stdout) console.log(stdout)
+      //if (stderr) console.log('shell error: ' + stderr)
+    }
+    exec(cmdd, errorfunction)
+
+    fs.writeFile('./models/user.js', 'const mongoose = require(\'mongoose\')\nconst Schema = mongoose.Schema\nconst userschema = new Schema({\n  name: {\n    type: String,\nrequired: [true, \'Name is reequiered\']\n  }\n})\n\nconst user = mongoose.model(\'usermodel\', userschema)\nmodule.exports = user', (err) => {
+      if (err) {
+        throw err
+      }
+    })
+
+    // creating db.js in config
+    const cdm = 'cd ..'
+    let errorfn = (error, stdout, stderr) => {
+      if (error) console.log('exec error: ' + error)
+      if (stdout) console.log(stdout)
+      //if (stderr) console.log('shell error: ' + stderr)
+    }
+    exec(cdm, errorfn)
+
+
+    const cdd = 'cd config'
+    let errorfunct = (error, stdout, stderr) => {
+      if (error) console.log('exec error: ' + error)
+      if (stdout) console.log(stdout)
+      //if (stderr) console.log('shell error: ' + stderr)
+    }
+    exec(cdd, errorfunct)
+
+    fs.writeFile('./config/db.js', 'module.exports = {\n  \'url\': "mongodb://ajojohn:ajojohn123@ds219641.mlab.com:19641/justblog"\n};', (err) => {
+      if (err) {
+        throw err
+      }
+    })
+
 
   })
 
