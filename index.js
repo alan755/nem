d3c20a6e67818d73d116c5c8aa40f5a4624447f9
alanmathew333 <mathewalan84@gmail.com>
2018-07-21 22:39:02 +0530

    increased functionality

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const program = require('commander')
 const inquirer = require('inquirer')
 const fs = require('fs')
 
-const exec = require('child_process').exec // to execute commands
+const exec = require('child_process').exec // to execute
 
 let listfunction = () => {
 
-  // questions
-  console.log('\n-{ N E M }- Node-Express-MongoDb -')
+
+  console.log('\n-{ N E M }-')
   console.log('\nPlease answer the following :-\n')
   inquirer.prompt([{
     name: 'name',
@@ -34,6 +34,16 @@ let listfunction = () => {
     type: 'input',
     message: 'Kindly specify the license (MIT) :'
   }, {
+    name: 'api',
+    type: 'list',
+    message: 'Which among the following architecture do you choose :-',
+    choices: ['RESTful API', 'GraphQL']
+  }, {
+    name: 'database',
+    type: 'list',
+    message: 'Which type of database :-',
+    choices: ['MongoDB', 'MySQL']
+  }, {
     name: 'dependencies',
     type: 'checkbox',
     message: 'Which all dependencies do you need :-',
@@ -47,22 +57,11 @@ let listfunction = () => {
       name: 'cors',
       checked: true
     }, {
-      name: 'mongoose',
-      checked: true
-    }, {
       name: 'ejs'
     }, {
       name: 'express-session'
     }, {
       name: 'cokkies'
-    }]
-  }, {
-    name: 'devdependencies',
-    type: 'checkbox',
-    message: 'Specify the dev dependencies :-',
-    choices: [{
-      name: 'nodemon',
-      checked: true
     }]
   }, {
     name: 'confirmation',
@@ -72,62 +71,127 @@ let listfunction = () => {
 
     const command = 'mkdir ' + answers.name
     let erfun = (error, stdout, stderr) => {
-      if (error) console.log('exec error: ' + error)
-      if (stdout) console.log(stdout)
-      //if (stderr) console.log('shell error: ' + stderr)
+      if (error) {
+        console.log('exec error: ' + error)
+      }
+      if (stdout) {
+        console.log(stdout)
+      }
+      if (stderr) {
+        console.log('shell error: ' + stderr)
+      }
     }
     exec(command, erfun)
 
+    // package.json creation
+
     const cdd = 'cd ' + answers.name
     let errfun = (error, stdout, stderr) => {
-      if (error) console.log('exec error: ' + error)
-      if (stdout) console.log(stdout)
-      //if (stderr) console.log('shell error: ' + stderr)
+      if (error) {
+        console.log('exec error: ' + error)
+      }
+      if (stdout) {
+        console.log(stdout)
+      }
+      if (stderr) {
+        console.log('shell error: ' + stderr)
+      }
     }
     exec(cdd, errfun)
 
-
-    // package.json creation
     fs.writeFile('./' + answers.name + '/package.json', '{\n  "name": "' + answers.name + '",\n  "version": "' + answers.version + '",\n  "description": "' + answers.description + '",\n  "main":"index.js",\n  "scripts": {\n    "test":""\n  },\n  "repository": {\n    "type": "",\n    "url": ""\n  },\n  "keywords": {\n  },\n  "author": "' + answers.author + '",\n  "license": "' + answers.license + '",\n  "bugs": {\n    "url": ""\n  },\n  "homepage": "",\n"devDependencies": {\n  },\n  "dependencies": {\n  }\n}', function (err) {
-      if (err) throw err;
+      if (err) {
+        throw err;
+      }
     })
 
 
-    // Adding dependencies
+    // Adding dependencies 
+
     setTimeout(() => {
 
       for (var i = 0; answers.dependencies[i] + '' != 'undefined'; i++) {
 
         const cmd = 'cd ' + answers.name + ' ; npm install --save ' + answers.dependencies[i]
         let errrfun = (error, stdout, stderr) => {
-          if (error) console.log('exec error: ' + error)
-          if (stdout) console.log(stdout)
-          //if (stderr) console.log('shell error: ' + stderr)
+          if (error) {
+            console.log('exec error: ' + error)
+          }
+          if (stdout) {
+            console.log(stdout)
+          }
+          if (stderr) {
+            console.log('shell error: ' + stderr)
+          }
         }
         exec(cmd, errrfun)
       }
+
+      if (answers.api + '' == 'GraphQL') {
+
+        const cmdd = 'cd ' + answers.name + ' ; npm install --save graphql express-graphql'
+        let errf = (error, stdout, stderr) => {
+          if (error) {
+            console.log('exec error: ' + error)
+          }
+          if (stdout) {
+            console.log(stdout)
+          }
+          if (stderr) {
+            console.log('shell error: ' + stderr)
+          }
+        }
+        exec(cmdd, errf)
+      }
+
+      if (answers.database + '' == 'MongoDB') {
+
+        const cmdd = 'cd ' + answers.name + ' ; npm install --save mongodb'
+        let errf = (error, stdout, stderr) => {
+          if (error) {
+            console.log('exec error: ' + error)
+          }
+          if (stdout) {
+            console.log(stdout)
+          }
+          if (stderr) {
+            console.log('shell error: ' + stderr)
+          }
+        }
+        exec(cmdd, errf)
+
+      } else {
+
+        const cmd = 'cd ' + answers.name + ' ;sudo npm install --save sequelize mysql'
+        let errrfun = (error, stdout, stderr) => {
+          if (error) {
+            console.log('exec error: ' + error)
+          }
+          if (stdout) {
+            console.log(stdout)
+          }
+          if (stderr) {
+            console.log('shell error: ' + stderr)
+          }
+        }
+        exec(cmd, errrfun)
+      }
+
     }, 1000)
 
-    //console.log('\nEnter your password :-\n')
-    // Adding dev dependencies
-
-    const cmdd = 'cd ' + answers.name + ' ; sudo npm install -g nodemon'
-    let errf = (error, stdout, stderr) => {
-      if (error) console.log('exec error: ' + error)
-      if (stdout) console.log(stdout)
-      //if (stderr) console.log('shell error: ' + stderr)
-    }
-    exec(cmdd, errf)
-
-
-
-
     // creating all directories
+
     const cdm = 'mkdir ./' + answers.name + '/routes ./' + answers.name + '/models ./' + answers.name + '/controller ./' + answers.name + '/config  ./' + answers.name + '/utils ./' + answers.name + '/services'
     let errfunction = (error, stdout, stderr) => {
-      if (error) console.log('exec error: ' + error)
-      if (stdout) console.log(stdout)
-      //if (stderr) console.log('shell error: ' + stderr)
+      if (error) {
+        console.log('exec error: ' + error)
+      }
+      if (stdout) {
+        console.log(stdout)
+      }
+      if (stderr) {
+        console.log('shell error: ' + stderr)
+      }
     }
     exec(cdm, errfunction)
 
@@ -135,19 +199,23 @@ let listfunction = () => {
 
     const com = 'cd ' + answers.name
     let errrfun = (error, stdout, stderr) => {
-      if (error) console.log('exec error: ' + error)
-      if (stdout) console.log(stdout)
-      //if (stderr) console.log('shell error: ' + stderr)
+      if (error) {
+        console.log('exec error: ' + error)
+      }
+      if (stdout) {
+        console.log(stdout)
+      }
+      if (stderr) {
+        console.log('shell error: ' + stderr)
+      }
     }
     exec(com, errrfun)
 
     fs.writeFile('./' + answers.name + '/server.js', 'const express = require(\'express\');\nconst bodyParser = require(\'body-parser\');\nconst cors = require(\'cors\');\nconst morgan = require(\'morgan\');\nconst mongoose = require(\'mongoose\');\nconst db = require(\'./config/db.js\');\n\nconst port = process.env.PORT || 8083;\n\nconst app = express();\n\napp.use(bodyParser.json());\napp.use(morgan(\'dev\'));\napp.use(cors());\n\nmongoose.connect(db.url);\n\n\napp.listen(port, () => {\n  console.log(`Server is up in port : ` + port);\n})', function (err) {
-      if (err) throw err;
+      if (err) {
+        throw err;
+      }
     })
-
-
-
-
 
   })
 
